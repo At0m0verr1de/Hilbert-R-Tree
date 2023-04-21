1d1cfa5c1111af271f22e1385a56851396c68cc6
Yash Pandey <109035687+yashpandey474@users.noreply.github.com>
2023-04-21 21:01:12 +0530

    Update hilbertRTreeUpdated.c

--- hilbertRTreeUpdated.c
+++ hilbertRTreeUpdated.c
@@ -10,6 +10,7 @@
 #define MIN_CHILDREN m
 #define MAX_POINTS 21
 int CURRENT_ID = 1;
+int num_results = 0;
 
 typedef struct Point Point;
 struct Point{
@@ -244,4 +245,32 @@ int main() {
     
 
     return 0;
+}
+
+
+// SEARCH ALGORITHM
+//-> NONLEAF - THOSE WITH MBR INTERSECTING THE QUERY WINDOW W
+//-> LEAF - THOSE WITH MBR INTERSECTING THE QUERY WINDOW W
+int num_results = 0;void search(NODE root, Rectangle rectangle, LeafEntry* results){
+    if(root->is_leaf == 1){
+        for(int i = 0 ; i<root->u.leaf_node.num_entries; i++){
+            if(intersects(root->u.leaf_node.entries[i].mbr, rectangle)){
+                results[num_results++] = root->u.leaf_node.entries[i];
+            }
+        }
+    }
+    else{
+        for(int i = 0; i<root->u.non_leaf_node.num_entries; i++){
+            if(intersects(root->u.non_leaf_node.entries[i].mbr, rectangle)){
+                search(root->u.non_leaf_node.entries[i].child_ptr, rectangle, results);
+            }
+        }
+    }
+}
+
+bool intersects(Rectangle r1, Rectangle r2){
+    return !(
+        r1.top_right.x < r2.bottom_left.x || r2.top_right.x < r1.bottom_left.x || 
+        r1.top_right.y < r2.bottom_left.y || r2.top_right.y < r1.bottom_left.y
+    );
 }
