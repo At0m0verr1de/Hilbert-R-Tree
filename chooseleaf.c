105bf3dc208297afec19681e970574b48de92373
Yash Pandey <109035687+yashpandey474@users.noreply.github.com>
2023-04-22 13:59:23 +0530

    Update chooseleaf.c

--- chooseleaf.c
+++ chooseleaf.c
@@ -10,24 +10,23 @@ NODE ChooseLeaf(NODE n, Rectangle r, int h){
     GREATER THAN H*/
     float min_LHV= INFINITY;
     NODE next_node = NULL;
-    for(int i = 0; i<MAX_CHILDREN; i++){
-        if(n->children[i] != NULL){
-            if(n->children[i]->lhv > h && n->children[i]->lhv < min_LHV){
-                min_LHV = n->children[i]->lhv;
-                next_node = n->children[i];
+    for(int i = 0; i<n->u.non_leaf_node.num_entries; i++){
+            if(n->u.non_leaf_node.entries[i].largest_hilbert_value > h && n->u.non_leaf_node.entries[i].largest_hilbert_value < min_LHV){
+                min_LHV = n->u.non_leaf_node.entries[i].largest_hilbert_value;
+                next_node = n->u.non_leaf_node.entries[i].child_ptr;
             }
-        }
+        
     }
     // IF ALL CHILDREN HAVE LHV LESS THAN H
     if(next_node == NULL){
         //CHOOSE THE CHILD NODE WITH LARGEST LHV
-        for(int i = 0; i<MAX_CHILDREN; i++){
-            if(n->children[i] != NULL){
-                if(n->children[i]->lhv > min_LHV){
-                    min_LHV = n->children[i]->lhv;
-                    next_node = n->children[i];
+        for(int i = 0; i<n->u.non_leaf_node.num_entries; i++){
+            
+                if(n->u.non_leaf_node.entries[i].largest_hilbert_value > min_LHV){
+                    min_LHV = n->u.non_leaf_node.entries[i].largest_hilbert_value;
+                    next_node = n->u.non_leaf_node.entries[i].child_ptr;
                 }
-            }
+            
         }
     }
 
